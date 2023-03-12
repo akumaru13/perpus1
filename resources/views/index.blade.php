4c70c993431c0e1056cded80131e908eb3e7d576
unknown <akmalhikmah13@gmail.com>
2023-03-12 21:41:53 +0900

    perpus

---
+++ resources/views/index.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.main')
+@section('title', 'Halaman Beranda')
+@section('content')
+<!-- PAGE CONTENT-->
+<div class="page-content--bgf7">
+
+    <!-- DATA TABLE-->
+    <section class="p-t-20">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="title-1 m-b-20"><strong>data table library buku</strong></h3>
+                    <div class="table-data__tool">
+
+
+                        <button class="">
+                            <a href="{{ route('login')}}" class="au-btn au-btn-icon au-btn--green au-btn--small">add buku</a>
+                        </button>
+
+                        <form class="au-form-icon--sm" action="/cari" method="GET">
+                            <input class="au-input--w300 au-input--style2" type="text" name="cari" placeholder="Search for buku" value="{{ old('cari')}}">
+                            <button class="au-btn--submit2" type="submit" value="CARI">
+                                <i class="zmdi zmdi-search"></i>
+                            </button>
+                        </form>
+
+
+                    </div>
+                    <div class="table-responsive table-responsive-data2">
+                        <table class="table table-data2">
+                            <thead>
+                                <tr>
+                                    <tr>
+                                        <th>No</th>
+                                        <th style="text-align: center; vertical-align: middle;">Gambar</th>
+                                        <th style="text-align: center; vertical-align: middle;">Judul</th>
+                                        <th style="text-align: center; vertical-align: middle;">Pengarang</th>
+                                        <th style="text-align: center; vertical-align: middle;">Penerbit</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($cruds as $index => $crud)   
+                                    <tr class="tr-shadow">
+                                        <td style="text-align: center; vertical-align: middle;">{{ $index + 1 }}</td>
+                                        <td style="text-align: center; vertical-align: middle;"><img src="{{asset('admin/images/buku/' . $crud->gambar)}}" width=200 alt=""></td>
+                                        <td style="text-align: center; vertical-align: middle;">{{ $crud->judul }}</td>
+                                        <td style="text-align: center; vertical-align: middle;">{{ $crud->pengarang }}</td>
+                                        <td style="text-align: center; vertical-align: middle;">{{ $crud->penerbit}}</td>
+                                    <td>
+                                        <div class="table-data-feature">
+                                            <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
+                                                <a class="zmdi zmdi-edit" href="{{ route('login')}}"></a>
+                                            </button>
+                                            <button href="#" class="item" data-toggle="tooltip" data-placement="top" title="Delete">
+                                                <a class="zmdi zmdi-delete" href="{{ route('login')}}"></a>
+                                            </button>
+                                        </div>
+                                    </td>
+                                    <tr class="spacer"></tr>
+                                </tr>
+                                 @endforeach
+                            </tbody>
+                        </table>
+                        {{ $cruds->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END DATA TABLE-->
+
+    <!-- COPYRIGHT-->
+    <section class="p-t-60 p-b-20">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright">
+                        <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END COPYRIGHT-->
+</div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header class="header-desktop3 d-none d-lg-block">
+    <div class="section__content section__content--p35">
+        <div class="header3-wrap">
+            
+            <div class="header__navbar">
+                <ul class="list-unstyled">
+                    
+                    <li class="has-sub">
+                        <a href="#">
+                            <i class="fas fa-copy"></i>
+                            <span class="bot-line"></span>Pages</a>
+                        <ul class="header3-sub-list list-unstyled">
+                            <li>
+                                <a href="{{route ('login')}}">Login</a>
+                            </li>
+                            <li>
+                                <a href="{{route ('register')}}">Register</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    
+                </ul>
+            </div>
+            <div class="header__tool">
+                <div class="header-button-item has-noti js-item-menu">
+                    
+                </div>
+                <div class="header-button-item js-item-menu">
+                    
+                </div>
+                <div class="account-wrap">
+                    <div class="account-item account-item--style2 clearfix js-item-menu">
+                        <div class="image">
+                            
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
